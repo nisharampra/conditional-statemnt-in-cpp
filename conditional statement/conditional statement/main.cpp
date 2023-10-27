582c4eed3ef8268e0010291782a351314e27c73f
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-10-27 22:36:06 +0800

    Add files via upload

---
+++ conditional statement/conditional statement/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  conditional statement
+//
+//  Created by Nisha Ramprasath on 26/10/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int roll;
+    cout<<"enter ur roll number";
+    cin>>roll;
+    if(roll<1){
+        cout<<"invalid roll";
+    }
+    
+    else{
+        cout<<"valid roll";
+
+    }
+    return 0;
+}
